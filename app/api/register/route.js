45c035a1a7dabe3081471999c704ba654877c7d2
NguyenDat2k1 <ntdat170401@gmail.com>
2023-10-09 16:56:09 +0700

    next update login register

---
+++ app/api/register/route.js
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import User from "@/models/user";
+import { connectMongoDB } from "@/lib/mongodb";
+import bcrypt from "bcryptjs";
+
+export async function POST(req) {
+    try {
+        const {name, email, password } = await req.json();
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await connectMongoDB();
+        await User.create({name,email, password: hashedPassword });
+
+        return NextResponse.json({ message: "user registered"}, {status: 201});
+    } catch (error) {
+        return NextResponse.json(
+            { message: "An error occurred while registering the user."},
+            {status: 500 }
+        )
+    }
+}
---
+++ components/UserInfo.jsx
@@ -0,0 +1,13 @@
+export default function UserInfo() {
+    return <div className="grid place-items-center h-screen">
+        <div className="shadow-lg p-8 bg-zince-300/10">
+            <div>
+                Name: <span className="font-bold">john</span>
+            </div>
+            <div>
+                Email:<span className="font-bold">john@gmail.com</span>
+            </div>
+            <button className="bg-red-500 text-white font-bold px-6 py-2 mt-3">Log out</button>
+            </div>
+        </div>
+}
