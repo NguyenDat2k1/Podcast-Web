2c4a50e528a4d4aef801888e411dccc2041745b1
NguyenDat2k1 <ntdat170401@gmail.com>
2023-10-12 17:15:10 +0700

    new navbar

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -1,12 +1,43 @@
 "use client";
 
+import Link from 'next/link';
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
+
 
 export default function UserInfo() {
   const { data: session } = useSession();
 
   return (
+    <nav className="bg-blue-500 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+          <p className="text-white text-2xl font-bold">Trang Chủ</p>
+        </Link>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/about">
+              <p className="text-white">Giới Thiệu</p>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/register"}>
+              <p className="text-white">Register</p>
+            </Link>
+          </li>
+          <li>
+          <button
+          onClick={() => signOut()}
+          className=" text-white "
+        >
+          Log Out
+        </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+  (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-8 bg-zince-300/10 flex flex-col gap-2 my-6">
         <div>
