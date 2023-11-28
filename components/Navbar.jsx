5070efe9de0f55d5d58da84e66db36fed9c0df54
Nguyễn Tiến Đạt <646626@sv.vnua.edu.vn>
2023-11-28 21:26:24 +0700

    search function

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,76 @@
 "use client";
 
+import { useState, useEffect } from "react";
+import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
-import { signOut } from "next-auth/react";
-import { useSession } from "next-auth/react";
-import { useState } from "react";
-import React from "react";
+import { redirect, useRouter } from "next/navigation";
 
 const Navbar = () => {
-  const { data: session } = useSession();
-
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   const [profileMenuOpen, setProfileMenuOpen] = useState(false);
+  const { data: session } = useSession();
+  const [updateFlag, setUpdateFlag] = useState(false);
+
+  const [listPodcast, setListPodcast] = useState([]);
+  const router = useRouter();
+  useEffect(() => {
+    const getListPodcast = async () => {
+      try {
+        const resListPodcast = await fetch("/api/getAllPodcast", {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+          },
+        });
+
+        const podcasts = await resListPodcast.json();
+
+        console.log(podcasts);
+
+        if (Array.isArray(podcasts)) {
+          setListPodcast(podcasts);
+        } else {
+          console.error("Invalid data structure in the response:");
+        }
+      } catch (error) {
+        console.error("Error fetching data from MongoDB:", error);
+      }
+    };
+
+    getListPodcast();
+  }, [updateFlag]);
+
+  // useEffect(() => {
+
+  //   setSearchResults([
+  //     { id: 1, title: "Podcast 1" },
+  //     { id: 2, title: "Podcast 2" },
+  //     // Thêm các podcast khác
+  //   ]);
+  // }, [searchTerm]);
+
+  useEffect(() => {
+    // Tìm kiếm trong danh sách podcast khi searchTerm thay đổi
+    const searchInList = () => {
+      const results = listPodcast.filter((podcast) =>
+        podcast.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setSearchResults(results);
+    };
+
+    searchInList();
+  }, [searchTerm, listPodcast]);
 
   const toggleProfileMenu = () => {
     setProfileMenuOpen(!profileMenuOpen);
   };
 
+  const handlePodcastClick = (event, level, title, id) => {
+    event.stopPropagation();
+
+    router.push(`/detailPage/${level}/${title}/${id}`);
+  };
   return (
     <div>
       <nav className="bg-blue-400 p-4">
@@ -22,29 +78,79 @@ const Navbar = () => {
           <Link href="/">
             <p className="text-white text-2xl font-bold">Podcast Site</p>
           </Link>
-          <div className="relative w-200">
+          <div className="relative flex-1">
             <input
               type="text"
               placeholder="Tìm kiếm..."
-              className="w-full border rounded py-1 px-2"
+              className="ml-20 w-3/4 border rounded py-1 px-2"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
-            <button className="absolute right-0 top-0 m-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-search"
-                viewBox="0 0 16 16"
-              >
-                <path d="M11.742 10.344a6.5 6.5 0 111.397-1.397h0l3.85 3.85a1 1 0 01-1.415 1.415l-3.85-3.85zm-.397 0a5.5 5.5 0 10-1.397 1.397h0l-3.85 3.85a1 1 0 11-1.415-1.415l3.85-3.85z" />
-              </svg>
-            </button>
+            {searchTerm && (
+              <div className="absolute left-20 top-full w-3/4 bg-white border rounded mt-1 p-2 overflow-hidden">
+                {/* Hàng đầu */}
+                <div className="flex justify-between mb-2">
+                  <Link href="/favouriteList">
+                    <div className="bg-blue-500 text-white px-2 py-1 rounded">
+                      A1
+                    </div>
+                  </Link>
+                  <Link href="/favouriteList">
+                    <div className="bg-blue-500 text-white px-2 py-1 rounded">
+                      A2
+                    </div>
+                  </Link>
+                  <Link href="/favouriteList">
+                    <div className="bg-blue-500 text-white px-2 py-1 rounded">
+                      B1
+                    </div>
+                  </Link>
+                  <Link href="/favouriteList">
+                    <div className="bg-blue-500 text-white px-2 py-1 rounded">
+                      B2
+                    </div>
+                  </Link>
+                  <Link href="/favouriteList">
+                    <div className="bg-blue-500 text-white px-2 py-1 rounded">
+                      C1
+                    </div>
+                  </Link>
+                  <Link href="/favouriteList">
+                    <div className="bg-blue-500 text-white px-2 py-1 rounded">
+                      C2
+                    </div>
+                  </Link>
+                </div>
+
+                {searchResults.map((podcast) => (
+                  <div
+                    key={podcast.id}
+                    className="mb-2 transition duration-300 ease-in-out transform hover:scale-105 pl-4 cursor-pointer"
+                    onClick={(event) =>
+                      handlePodcastClick(
+                        event,
+                        podcast.level,
+                        podcast.name,
+                        podcast.id
+                      )
+                    }
+                  >
+                    <p className="text-blue-400 font-bold hover:text-blue-700">
+                      {podcast.name}
+                    </p>
+                    <p className="hover:text-blue-700">
+                      Level: {podcast.level}
+                    </p>
+                    <p className="hover:text-blue-700">Type: {podcast.type}</p>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
           <ul className="flex space-x-4">
             <li className="relative">
               <Link href="/favouriteList">
-                <button className="text-white border border-blue-400 rounded px-2 py-1 mr-2">
+                <button className="text-white border border-blue-400 rounded px-2 py-1">
                   Favourite List
                 </button>
               </Link>
@@ -52,7 +158,7 @@ const Navbar = () => {
             <li className="relative">
               {session?.user?.name === "Admin" && (
                 <Link href="/listType">
-                  <button className="text-white border border-blue-400 rounded px-2 py-1 mr-2">
+                  <button className="text-white border border-blue-400 rounded px-2 py-1">
                     Thống Kê
                   </button>
                 </Link>
@@ -61,13 +167,12 @@ const Navbar = () => {
             <li className="relative">
               {session?.user?.name === "Admin" && (
                 <Link href="/listType">
-                  <button className="text-white border border-blue-400 rounded px-2 py-1 mr-2">
+                  <button className="text-white border border-blue-400 rounded px-2 py-1">
                     Create New Podcast
                   </button>
                 </Link>
               )}
             </li>
-
             <li className="relative">
               <button
                 className="text-white pt-1.5"
@@ -80,16 +185,12 @@ const Navbar = () => {
                   <Link href="/profile">
                     <div className="p-2 hover:bg-blue-700">Trang Cá Nhân</div>
                   </Link>
-                  {/* <div
-                    className="p-2 hover-bg-blue-700 cursor-pointer"
+                  <div
+                    className="p-2 hover:bg-blue-700 cursor-pointer"
                     onClick={() => signOut()}
-                   
                   >
-                    Log Out
-                  </div> */}
-                  <Link href="/login">
-                    <div className="p-2 hover:bg-blue-700">Logout</div>
-                  </Link>
+                    Logout
+                  </div>
                 </div>
               )}
             </li>
